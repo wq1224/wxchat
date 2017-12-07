87dd3dbe24b899a444f8948ec24797cd6cf8dfca
Joice Wang <joice.wang@sap.com>
2017-12-07 18:02:57 +0800

    add xunfei call

--- test_group.py
+++ test_group.py
@@ -8,12 +8,14 @@ import hashlib
 
 base_url = "https://api.xfyun.cn"
 
-payload = "text=5LuK5aSp5pif5pyf5Yeg"
+payload_text = "text=5LuK5aSp5pif5pyf5Yeg"
+payload = {"text": "5LuK5aSp5pif5pyf5Yeg"}
 
 appid = "5a27a80b"
+app_key = "59f27e4c48e04dfdbb8fcef29c8ec157"
 cur_time = int(time.time())
 param = "eyJzY2VuZSI6Im1haW4iLCAidXNlcmlkIjoidXNlcl8wMDAxIn0="
-checkSum = appid + str(cur_time) + param + payload
+checkSum = app_key + str(cur_time) + param + payload_text
 
 header = {
 	"X-Appid": appid,
