e2b2a0f09f0c37c9ee3f0b4eb4af268e0db8fadc
Joice Wang <joice.wang@sap.com>
2018-04-23 17:29:11 +0800

    update to db file

--- test_update_data_to_db.py
+++ test_update_data_to_db.py
@@ -21,32 +21,48 @@ store_first_turn_member_num = "first_turn_member_num"
 date_format = "%Y-%m-%d %H:%M:%S"
 date_interval = 900
 
+base_api_url = "http://106.14.0.107:80/angular/"
 
+def get_user_info(user_remark_name):
+	result = requests.get(base_api_url + "getWechatUser?wechatUserId=" + user_remark_name)
+	if  result.text.strip(): 
+		return result.json()
+	else:
+		return
 
-def get_user_id(pinyin):
-	user_name = re.sub('\W+','', pinyin)
-	return str(int(time.time())) + user_name
+def update_user_info(user_info):
+	change_user_info(user_info,"u")
 
-def change_record(remark_name, nick_name):
-	file = os.path.join(os.getcwd()+os.path.sep+user_path+os.path.sep+nick_name+".conf")
-	conf = configparser.ConfigParser()
-	if os.path.exists(file) :
-		conf.read(file)
-		conf.set(store_section, store_name, remark_name)
-		conf.set(store_section, store_nick_name, nick_name)
-		conf.write(open(file,"w"))
-		file_desc = os.path.join(os.getcwd()+os.path.sep+user_path+os.path.sep+remark_name+".conf")
-		os.rename(file, file_desc)
+def insert_user_info(user_info):
+	change_user_info(user_info, "i")
 
+def change_user_info(user_info,sql_type):
+	url = base_api_url + "updateUserDB?wechatUserId=" + user_info[store_name]
+	url = url + "&type=" + sql_type
+	keys = user_info.keys()
+	if store_nick_name in keys:
+		url = url + "&userName=" + user_info[store_nick_name]
+	if store_first_turn_member_num in keys and user_info[store_first_turn_member_num] != 0:
+		url = url + "&groupMember=" + str(user_info[store_first_turn_member_num])
+	if store_last_file in keys:
+		url = url + "&jingWenJinDu=" + str(user_info[store_last_file])
+	result = requests.get(url)
 
-github_url = "http://106.14.0.107:8080/angular/getWechatUser?wechatUserId=test"
 #data = json.dumps({'name':'test', 'description':'some test repo'}) 
-r = requests.get(github_url)
-if  r.text.strip():  
-	result = r.json()
-	print(result["wechatUserId"])
-	print(result["jinDu"])
-	print(result["updateTime"])
+for file in os.listdir(user_path):
+	if "conf" in file :
+		conf = configparser.ConfigParser()
+		conf.read(os.path.join(os.getcwd()+os.path.sep+user_path+os.path.sep+file))
+		values = dict(conf.items(store_section))
+		result = {}
+		for key in values.keys():
+			result[key] = values[key]
+		username = os.path.splitext(file)[0]
+		r = get_user_info(username)
+		if  r:  
+			update_user_info(result)
+		else:
+			insert_user_info(result)
 	
 
 
